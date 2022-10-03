55b40b0a879e360c959477757f098d53f8dddc59
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-03 17:26:24 +0530

    Foreteller Cards

--- src/pages/AboutBitcoin.js
+++ src/pages/AboutBitcoin.js
@@ -1,43 +1,34 @@
-import React, { Fragment } from "react";
+import React, {Fragment} from "react";
 import MetaTags from "react-meta-tags";
 import LayoutTwo from "../layouts/LayoutTwo";
 import Breadcrumb from "../components/breadcrumbs/Breadcrumb";
 import MarketTable from "../containers/market-tables/MarketTable";
-// import AboutContentTwo from "../components/about-contents/AboutContentTwo";
-// import VideoPopup from "../components/video-popups/VideoPopup";
-// import CurrencyTicker from "../containers/currency-tickers/CurrencyTicker";
-// import AboutContentThree from "../components/about-contents/AboutContentThree";
+import ForetellerCard from "../containers/currency-forteller/ForetellerCard";
 
 const AboutBitcoin = () => {
-  return (
-    <Fragment>
-      <MetaTags>
-        <title>Howard | About Bitcoin</title>
-        <meta
-          name="description"
-          content="About bitcoin page of React JS Crypto Currency Template."
-        />
-      </MetaTags>
-      <LayoutTwo theme="white">
+    return (
+        <Fragment>
+            <MetaTags>
+                <title>Howard | About Bitcoin</title>
+                <meta
+                    name="description"
+                    content="About bitcoin page of React JS Crypto Currency Template."
+                />
+            </MetaTags>
+            <LayoutTwo theme="white">
 
-        {/* breadcrumb */}
-        <Breadcrumb title="Coins" />
+                {/* breadcrumb */}
+                <Breadcrumb title="Coins"/>
 
-        {/* about content */}
-        {/*<AboutContentThree />*/}
+                {/* market table */}
+                <MarketTable/>
 
-        {/* market table */}
-        <MarketTable />
+                {/*Foreteller Cards List*/}
+                <ForetellerCard/>
 
-        {/* currency ticker */}
-        {/*<CurrencyTicker theme="dark" />*/}
-        {/* video popup */}
-        {/*<VideoPopup />*/}
-        {/* about content two */}
-        {/*<AboutContentTwo />*/}
-      </LayoutTwo>
-    </Fragment>
-  );
+            </LayoutTwo>
+        </Fragment>
+    );
 };
 
 export default AboutBitcoin;
