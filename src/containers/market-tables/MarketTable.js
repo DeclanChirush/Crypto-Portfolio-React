b096dd864884dce4c9186c51f592addcdf155c44
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-03 15:47:06 +0530

    Coin Table Finish

--- src/containers/market-tables/MarketTable.js
+++ src/containers/market-tables/MarketTable.js
@@ -1,43 +1,75 @@
-import React from "react";
-import marketTableData from "../../data/market-tables/market-table-one.json";
-import MarketTableSingle from "../../components/market-tables/MarketTableSingle.js";
+import React, {useEffect, useState} from "react";
+
+import MarketService from "../../services/MarketService";
+import SectionTitleTwo from "../../components/ui/section-titles/SectionTitleTwo";
+import TableSample from "./TableSample";
 
 const MarketTable = () => {
-  return (
-    <div className="market__table bg--white pt--140 pb--120">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12 table-responsive">
-            <table className="mkt__pre__list">
-              <thead>
-                <tr>
-                  <th className="market-no">
-                    <span className="nobr">#No</span>
-                  </th>
-                  <th className="market-symbol">Name &amp; SYmble</th>
-                  <th className="market-prize">
-                    <span className="nobr">Price</span>
-                  </th>
-                  <th className="market-volume">
-                    <span className="nobr">Volume</span>
-                  </th>
-                  <th className="market-dat">
-                    <span className="nobr">%7day</span>
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {marketTableData &&
-                  marketTableData.map((single, key) => {
-                    return <MarketTableSingle data={single} key={key} />;
-                  })}
-              </tbody>
-            </table>
-          </div>
+
+    const [marketData, setMarketData] = useState([]);
+
+    useEffect(() => {
+        async function dataFetch() {
+            MarketService.getCoinDta()
+                .then(response => response.data)
+                .then((data) => {
+                    let newArray = data.slice(0, 5);
+                    setMarketData(newArray);
+                    console.log(newArray);
+                }).catch(error => console.log(error.message));
+        }
+
+        dataFetch();
+    }, []);
+
+
+    return (<div className="market__table bg--white pt--140 pb--120">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <SectionTitleTwo title="CRYPTO CURRENCY PRICES"/>
+                    <p className='text-center'>The Bitcoin ecosystem at a glance. Any of our service with free test credits or actual
+                        Bitcoins.</p>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-lg-12 table-responsive">
+                    <table className="mkt__pre__list">
+                        <thead>
+                        <tr>
+                            <th className="market-no text-center">
+                                <span className="nobr">Name</span>
+                            </th>
+                            <th className="market-symbol text-center">Symbol</th>
+                            <th className="market-prize text-center">
+                                <span className="nobr">USD Price</span>
+                            </th>
+                            <th className="market-volume text-center">
+                                <span className="nobr">Change 24h</span>
+                            </th>
+                            <th className="market-dat text-center">
+                                <span className="nobr">Change 7d</span>
+                            </th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {marketData.length === 0 ?
+                            <TableSample/>
+                            : marketData.map((data, key) => (
+                                <tr key={key}>
+                                    <td className="market-symbol text-uppercase text-center">{data.symbol}</td>
+                                    <td className="market-no text-center">{data.name}</td>
+                                    <td className="market-prize text-center">$ {(data.market_data.current_price.usd).toFixed(2)}</td>
+                                    <td className="market-volume text-center">{(data.market_data.price_change_percentage_24h).toFixed(2)} %</td>
+                                    <td className="market-dat text-center">{(data.market_data.price_change_percentage_7d).toFixed(2)} %</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    </div>);
 };
 
 export default MarketTable;
