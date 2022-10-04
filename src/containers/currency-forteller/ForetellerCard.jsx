57aa1bdc631cd788a76fb6e50343dad38efc3cac
Navinda Chandrasiri <adilanga@gmail.com>
2022-10-04 12:32:29 +0530

    Currency List Updated

--- src/containers/currency-forteller/ForetellerCard.jsx
+++ src/containers/currency-forteller/ForetellerCard.jsx
@@ -1,73 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import {Card, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
-import CurrencyService from "../../services/CurrencyService";
+import CurrencyDetailsService from "../../services/CurrencyDetailsService";
 
 import SectionTitleTwo from "../../components/ui/section-titles/SectionTitleTwo";
 
 const ForetellerCard = () => {
-    const [cardData, setCardData] = useState(dataCards);
-    const [foretellerData, setForetellerData] = useState([]);
-
+    const [cardData, setCardData] = useState([]);
 
     useEffect(() => {
         async function dataFetch() {
-            CurrencyService.getPrediction()
+            await CurrencyDetailsService.getAllCurrency()
                 .then(response => response.data)
-                .then(data => data.data)
                 .then((data) => {
-                    setForetellerData(data);
+                    setCardData(data);
                 }).catch(error => console.log(error.message));
         }
-
 
         dataFetch();
     }, []);
 
-    let dataCards = [
-        {
-            "name": "BitCoin",
-            "code": "BTC_USD",
-            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1024px-Bitcoin.svg.png",
-            "path": "/foreteller-bitcoin"
-        },
-        {
-            "name": "Ethereum",
-            "code": "ETH_USD",
-            "image": "https://yt3.ggpht.com/ytc/AMLnZu8fhtd4JmRi9ALE3-IppCAEuCNPZJG1eBcM9UYU2g=s900-c-k-c0x00ffffff-no-rj",
-            "path": "/foreteller-ethereum"
-        },
-        {
-            "name": "PolkaEx",
-            "code": "PKEX_USD",
-            "image": "https://icodrops.com/wp-content/uploads/2021/09/IMG_20210929_113644_788.jpg",
-            "path": "/foreteller-polkaex"
-        },
-    ]
-
     const text = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
 
-    return (
-        <div className="market__table bg--white pt--140 pb--120">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <SectionTitleTwo title="CURRENCY FORETELLER"/>
-                        <p className='text-center'>{text}</p>
-                    </div>
+    return (<div className="market__table bg--white pt--140 pb--120">
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-12">
+                    <SectionTitleTwo title="CURRENCY FORETELLER"/>
+                    <p className='text-center'>{text}</p>
                 </div>
+            </div>
 
-                <div className='pt-5'>
-                    <Row className="g-4 align-items-center justify-content-center">
-                        {
-                            dataCards.map((data, idx) => (
+            <div className='pt-5'>
+                <Row className="g-4 align-items-center justify-content-center">
+                    {
+                        cardData.length === 0 ?
+                            <div></div>
+                            :
+                            cardData.map((data, idx) => (
                                 <Link
                                     to={{
                                         pathname: process.env.PUBLIC_URL + '/foreteller',
                                         state: {
-                                            name: data.name,
-                                            code: data.code,
-                                            image: data.image
+                                            name: data.name, code: data.code, image: data.image
                                         }
                                     }}
                                     key={idx}>
@@ -83,13 +58,12 @@ const ForetellerCard = () => {
                                     </Card>
                                 </Link>
                             ))
-                        }
-                    </Row>
-                </div>
-
+                    }
+                </Row>
             </div>
+
         </div>
-    );
+    </div>);
 }
 
 export default ForetellerCard;
