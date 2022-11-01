e4f9c25cd4049368a1fbb777dbd3fc5d074a762f
Navinda Chandrasiri <adilanga@gmail.com>
2022-11-02 00:11:59 +0530

    Cus Navbar Update

--- src/components/dashboard/customer/NavbarCustomer.jsx
+++ src/components/dashboard/customer/NavbarCustomer.jsx
@@ -21,9 +21,9 @@ function NavbarCustomer(props) {
                         <li className="nav-item">
                             <Link to={'/customer-dashboard'} className={'nav-link text-white'}> Dashboard </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link to={'/customer-profile'} className={'nav-link text-white'}> Profile </Link>
-                        </li>
+                        {/*<li className="nav-item">*/}
+                        {/*    <Link to={'/customer-profile'} className={'nav-link text-white'}> Profile </Link>*/}
+                        {/*</li>*/}
                         <li className="nav-item">
                             <Link className={'nav-link text-white'} onClick={UserService.logout}> Logout </Link>
                         </li>
